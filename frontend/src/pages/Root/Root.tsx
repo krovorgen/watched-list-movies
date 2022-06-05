eaa66b7e68a702fb6df6e763e15988fa5f091697
maksim <krovorgen@gmail.com>
2022-06-05 22:12:03 +0300

    заготовка главного экрана

--- frontend/src/pages/Root/Root.tsx
+++ frontend/src/pages/Root/Root.tsx
@@ -2,12 +2,26 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { AppRoutes } from '../../helpers/routes';
 
+import styles from './Root.module.scss';
+
 export const Root = () => {
   return (
-    <>
-      <Link to={AppRoutes.Films}>Films</Link>
-      <Link to={AppRoutes.Serials}>Serials</Link>
-      <Link to={AppRoutes.Cartoons}>Cartoon</Link>
-    </>
+    <ul className={styles.items}>
+      <li className={styles.item}>
+        <Link className={styles.link} to={AppRoutes.Films}>
+          Films
+        </Link>
+      </li>
+      <li className={styles.item}>
+        <Link className={styles.link} to={AppRoutes.Serials}>
+          Serials
+        </Link>
+      </li>
+      <li className={styles.item}>
+        <Link className={styles.link} to={AppRoutes.Cartoons}>
+          Cartoon
+        </Link>
+      </li>
+    </ul>
   );
 };
