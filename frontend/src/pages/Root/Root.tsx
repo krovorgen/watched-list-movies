5095b78983925be84700312ae746c500970c7f22
maksim <krovorgen@gmail.com>
2022-06-06 23:48:45 +0300

    refactor

--- frontend/src/pages/Root/Root.tsx
+++ frontend/src/pages/Root/Root.tsx
@@ -2,11 +2,29 @@ import React, { MouseEvent, useCallback } from 'react';
 import { Link } from 'react-router-dom';
 
 import { AppRoutes } from 'helpers/routes';
-import misfits from 'assets/videos/misfits.webm';
 import legend from 'assets/videos/legend.webm';
+import misfits from 'assets/videos/misfits.webm';
 import walle from 'assets/videos/walle.webm';
 
 import styles from './Root.module.scss';
+
+const linksData = [
+  {
+    videoPath: legend,
+    linkPath: AppRoutes.Films,
+    linkTitle: 'Films',
+  },
+  {
+    videoPath: misfits,
+    linkPath: AppRoutes.Serials,
+    linkTitle: 'Serials',
+  },
+  {
+    videoPath: walle,
+    linkPath: AppRoutes.Cartoons,
+    linkTitle: 'Cartoon',
+  },
+];
 
 export const Root = () => {
   const mouseOverVideoHandler = useCallback(async (e: MouseEvent<HTMLLIElement>) => {
@@ -19,33 +37,17 @@ export const Root = () => {
 
   return (
     <ul className={styles.items}>
-      <li
-        className={styles.item}
-        onMouseOver={mouseOverVideoHandler}
-        onMouseOut={mouseOutVideoHandler}>
-        <video className={styles.video} src={legend} loop muted />
-        <Link className={styles.link} to={AppRoutes.Films}>
-          Films
-        </Link>
-      </li>
-      <li
-        className={styles.item}
-        onMouseOver={mouseOverVideoHandler}
-        onMouseOut={mouseOutVideoHandler}>
-        <video className={styles.video} src={misfits} loop muted />
-        <Link className={styles.link} to={AppRoutes.Serials}>
-          Serials
-        </Link>
-      </li>
-      <li
-        className={styles.item}
-        onMouseOver={mouseOverVideoHandler}
-        onMouseOut={mouseOutVideoHandler}>
-        <video className={styles.video} src={walle} loop muted />
-        <Link className={styles.link} to={AppRoutes.Cartoons}>
-          Cartoon
-        </Link>
-      </li>
+      {linksData.map((item) => (
+        <li
+          className={styles.item}
+          onMouseOver={mouseOverVideoHandler}
+          onMouseOut={mouseOutVideoHandler}>
+          <video className={styles.video} src={item.videoPath} loop muted />
+          <Link className={styles.link} to={item.linkPath}>
+            {item.linkTitle}
+          </Link>
+        </li>
+      ))}
     </ul>
   );
 };
