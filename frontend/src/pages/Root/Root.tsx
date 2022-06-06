fb0bd02515b87e7cb50b0e96248367c9f4afe42f
maksim <krovorgen@gmail.com>
2022-06-06 22:11:06 +0300

    video bg callbacks handler

--- frontend/src/pages/Root/Root.tsx
+++ frontend/src/pages/Root/Root.tsx
@@ -1,23 +1,47 @@
-import React from 'react';
+import React, { MouseEvent, useCallback } from 'react';
 import { Link } from 'react-router-dom';
+
 import { AppRoutes } from '../../helpers/routes';
+import misfits from '../../assets/videos/misfits.webm';
+import legend from '../../assets/videos/legend.webm';
+import walle from '../../assets/videos/walle.webm';
 
 import styles from './Root.module.scss';
 
 export const Root = () => {
+  const mouseOverVideoHandler = useCallback(async (e: MouseEvent<HTMLLIElement>) => {
+    await (e.currentTarget.children.item(0) as HTMLVideoElement).play();
+  }, []);
+
+  const mouseOutVideoHandler = useCallback((e: MouseEvent<HTMLLIElement>) => {
+    (e.currentTarget.children.item(0) as HTMLVideoElement).pause();
+  }, []);
+
   return (
     <ul className={styles.items}>
-      <li className={styles.item}>
+      <li
+        className={styles.item}
+        onMouseOver={mouseOverVideoHandler}
+        onMouseOut={mouseOutVideoHandler}>
+        <video className={styles.video} src={legend} loop muted />
         <Link className={styles.link} to={AppRoutes.Films}>
           Films
         </Link>
       </li>
-      <li className={styles.item}>
+      <li
+        className={styles.item}
+        onMouseOver={mouseOverVideoHandler}
+        onMouseOut={mouseOutVideoHandler}>
+        <video className={styles.video} src={misfits} loop muted />
         <Link className={styles.link} to={AppRoutes.Serials}>
           Serials
         </Link>
       </li>
-      <li className={styles.item}>
+      <li
+        className={styles.item}
+        onMouseOver={mouseOverVideoHandler}
+        onMouseOut={mouseOutVideoHandler}>
+        <video className={styles.video} src={walle} loop muted />
         <Link className={styles.link} to={AppRoutes.Cartoons}>
           Cartoon
         </Link>
