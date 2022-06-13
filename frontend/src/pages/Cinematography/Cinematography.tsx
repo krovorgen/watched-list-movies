06d010af37143ae11aa5f77390cb9dac87f45120
maksim <krovorgen@gmail.com>
2022-06-13 21:14:17 +0300

    added sorting

--- frontend/src/pages/Cinematography/Cinematography.tsx
+++ frontend/src/pages/Cinematography/Cinematography.tsx
@@ -33,11 +33,36 @@ type Props = {
   title: string;
 };
 
+enum SortTableValue {
+  rating = 'rating',
+  type = 'type',
+}
+
+export enum SortTableType {
+  complete = 0,
+  inProgress = 1,
+  waiting = 2,
+}
+
 export const Cinematography: FC<Props> = memo(({ currentType, title }) => {
   const [searchValue, setSearchValue] = useState('');
   const [isLoadingContent, setIsLoadingContent] = useState(false);
-  let [content, setContent] = useState<DataType[]>([]);
+  let [data, setData] = useState<DataType[]>([]);
   const [currentRow, setCurrentRow] = useState<DataType | null>(null);
+
+  const [sortKey, setSortKey] = useState<SortTableValue | undefined>(undefined);
+  const [isSortedDesc, setIsSortedDesc] = useState<boolean | undefined>(undefined);
+  const handleSort = useCallback(
+    (key: SortTableValue) => {
+      setSortKey(key);
+      if (isSortedDesc !== undefined) {
+        setIsSortedDesc(!isSortedDesc ? undefined : false);
+      } else {
+        setIsSortedDesc(true);
+      }
+    },
+    [isSortedDesc],
+  );
 
   const [isAddContentModal, setIsAddContentModal] = useState(false);
   const handleAddContent = useCallback(() => setIsAddContentModal((v) => !v), []);
@@ -60,7 +85,7 @@ export const Cinematography: FC<Props> = memo(({ currentType, title }) => {
     if (!result) return;
     try {
       await api.delete(id);
-      setContent((v) => v.filter((item) => item._id !== id));
+      setData((v) => v.filter((item) => item._id !== id));
     } catch ({ response }) {
       catchHandler(response);
     } finally {
@@ -73,58 +98,24 @@ export const Cinematography: FC<Props> = memo(({ currentType, title }) => {
   }, [currentRow, isEditContentModal]);
 
   if (searchValue.trim() !== '') {
-    content = content.filter((article) =>
+    data = data.filter((article) =>
       article.title.toLowerCase().includes(searchValue.toLowerCase()),
     );
   }
-
-  const moviesListMemo = useMemo(() => {
-    return content?.map((row) => (
-      <Table.TRow className={styles.tr} key={row._id}>
-        <Table.TCell>{row.title}</Table.TCell>
-        <Table.TCell>
-          {row.statusText ? (
-            <Tooltip
-              content={row.statusText}
-              position="top"
-              view="hint"
-              targetClassName={styles.tooltip}>
-              {iconStatus[row.status]}
-            </Tooltip>
-          ) : (
-            iconStatus[row.status]
-          )}
-        </Table.TCell>
-        <Table.TCell>{row.status === 'complete' ? row.rating : `—`}</Table.TCell>
-        <Table.TCell>
-          {row.linkKinopoisk ? (
-            <a className={styles.link} href={row.linkKinopoisk} target="_blank" rel="noreferrer">
-              <img src={kinopoisk} width={20} height={20} alt="kinopoisk" />
-            </a>
-          ) : null}
-          {row.linkTikTok ? (
-            <a className={styles.link} href={row.linkTikTok} target="_blank" rel="noreferrer">
-              <img src={tiktok} width={20} height={20} alt="tiktok" />
-            </a>
-          ) : null}
-        </Table.TCell>
-        <Table.TCell className={styles.nav}>
-          <Button
-            size="xxs"
-            view="secondary"
-            onClick={() => editCinematography(row)}
-            rightAddons={<EditMBlackIcon />}
-          />
-          <Button
-            size="xxs"
-            view="primary"
-            rightAddons={<DeleteSWhiteIcon />}
-            onClick={() => deleteCinematography(row._id, row.title)}
-          />
-        </Table.TCell>
-      </Table.TRow>
-    ));
-  }, [content, editCinematography, deleteCinematography]);
+  const sortedData = useMemo(() => {
+    if (!sortKey || isSortedDesc === undefined) return data;
+    return [...data].sort((a, b) => {
+      if (sortKey === SortTableValue.rating) {
+        return isSortedDesc ? b.rating - a.rating : a.rating - b.rating;
+      }
+      if (sortKey === SortTableValue.type) {
+        return isSortedDesc
+          ? SortTableType[b.status] - SortTableType[a.status]
+          : SortTableType[a.status] - SortTableType[b.status];
+      }
+      return 0;
+    });
+  }, [data, isSortedDesc, sortKey]);
 
   useEffect(() => {
     (async () => {
@@ -132,7 +123,7 @@ export const Cinematography: FC<Props> = memo(({ currentType, title }) => {
       try {
         if (isAddContentModal || isEditContentModal) return;
         const res = await api.get(currentType);
-        setContent(res.data);
+        setData(res.data);
       } catch ({ response }) {
         catchHandler(response);
       } finally {
@@ -142,7 +133,7 @@ export const Cinematography: FC<Props> = memo(({ currentType, title }) => {
   }, [isAddContentModal, isEditContentModal, currentType]);
 
   return (
-    <>
+    <div className={styles.root}>
       {isLoadingContent && <GlobalLoader />}
       <GoHome />
       <Typography.Title view="xlarge" tag="h1" className={styles.title}>
@@ -155,12 +146,20 @@ export const Cinematography: FC<Props> = memo(({ currentType, title }) => {
       <Table>
         <Table.THead>
           <Table.THeadCell>Название</Table.THeadCell>
-          <Table.THeadCell width={100} textAlign="center">
+          <Table.TSortableHeadCell
+            width={100}
+            textAlign="center"
+            isSortedDesc={sortKey === SortTableValue.type ? isSortedDesc : undefined}
+            onSort={() => handleSort(SortTableValue.type)}>
             Статус
-          </Table.THeadCell>
-          <Table.THeadCell width={100} textAlign="center">
+          </Table.TSortableHeadCell>
+          <Table.TSortableHeadCell
+            width={100}
+            textAlign="center"
+            isSortedDesc={sortKey === SortTableValue.rating ? isSortedDesc : undefined}
+            onSort={() => handleSort(SortTableValue.rating)}>
             Оценка
-          </Table.THeadCell>
+          </Table.TSortableHeadCell>
           <Table.THeadCell width={100} textAlign="center">
             Ссылка
           </Table.THeadCell>
@@ -168,7 +167,57 @@ export const Cinematography: FC<Props> = memo(({ currentType, title }) => {
             Управление
           </Table.THeadCell>
         </Table.THead>
-        <Table.TBody>{content.length > 0 && moviesListMemo}</Table.TBody>
+        <Table.TBody>
+          {sortedData?.map((row) => (
+            <Table.TRow className={styles.tr} key={row._id}>
+              <Table.TCell>{row.title}</Table.TCell>
+              <Table.TCell>
+                {row.statusText ? (
+                  <Tooltip
+                    content={row.statusText}
+                    position="top"
+                    view="hint"
+                    targetClassName={styles.tooltip}>
+                    {iconStatus[row.status]}
+                  </Tooltip>
+                ) : (
+                  iconStatus[row.status]
+                )}
+              </Table.TCell>
+              <Table.TCell>{row.status === 'complete' ? row.rating : `—`}</Table.TCell>
+              <Table.TCell>
+                {row.linkKinopoisk ? (
+                  <a
+                    className={styles.link}
+                    href={row.linkKinopoisk}
+                    target="_blank"
+                    rel="noreferrer">
+                    <img src={kinopoisk} width={20} height={20} alt="kinopoisk" />
+                  </a>
+                ) : null}
+                {row.linkTikTok ? (
+                  <a className={styles.link} href={row.linkTikTok} target="_blank" rel="noreferrer">
+                    <img src={tiktok} width={20} height={20} alt="tiktok" />
+                  </a>
+                ) : null}
+              </Table.TCell>
+              <Table.TCell className={styles.nav}>
+                <Button
+                  size="xxs"
+                  view="secondary"
+                  onClick={() => editCinematography(row)}
+                  rightAddons={<EditMBlackIcon />}
+                />
+                <Button
+                  size="xxs"
+                  view="primary"
+                  rightAddons={<DeleteSWhiteIcon />}
+                  onClick={() => deleteCinematography(row._id, row.title)}
+                />
+              </Table.TCell>
+            </Table.TRow>
+          ))}
+        </Table.TBody>
       </Table>
       <AddContentModal
         isAddContentModal={isAddContentModal}
@@ -182,6 +231,6 @@ export const Cinematography: FC<Props> = memo(({ currentType, title }) => {
           content={currentRow}
         />
       )}
-    </>
+    </div>
   );
 });
